fa620abed75af2054a942e8637fe9ad0b3ff454b
sofy <bedlusofi@gmail.com>
2023-07-26 16:23:31 +0300

    task-3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+define a class LRUCache that inherits from BaseCaching
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    represent a caching system
+    """
+
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item
+        value for the key key
+        """
+        if key is None or item is None:
+            return
+        if key in self.order:
+            self.cache_data[key] = item
+            self.order.remove(key)
+
+        else:
+            if len(self.order) >= BaseCaching.MAX_ITEMS:
+                key_value = self.order.pop(0)
+                del self.cache_data[key_value]
+                print("DISCARD {}".format(key_value))
+            self.cache_data[key] = item
+
+        self.order.append(key)
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key
+        """
+        if key not in self.order:
+            return None
+        return self.cache_data[key]

52666a1f859642e626b336982887c01ff6f6df21
sofy <bedlusofi@gmail.com>
2023-07-30 08:35:15 +0300

    update end_index

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,6 +10,6 @@ def index_range(page, page_size):
     """
 
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    end_index = start_index + page_size - 1
 
     return (start_index, end_index)

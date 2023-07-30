4ad4f38eb3c13f990a0c8683276606a914eceaf3
sofy <bedlusofi@gmail.com>
2023-07-30 08:39:44 +0300

    type-annotated

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,14 +2,15 @@
 """
 define a function called index_range
 """
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two containing a start
     index and an end index
     """
 
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
+    end_index = start_index + page_size
 
     return (start_index, end_index)

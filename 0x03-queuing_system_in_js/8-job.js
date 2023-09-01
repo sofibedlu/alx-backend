da6bded49eb7c207bbe929d07d9843334291d161
sofy <bedlusofi@gmail.com>
2023-09-01 14:27:04 +0300

    task-9

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,30 @@
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+  // Process each job in the array
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData);
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed: ${err}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+
+    job.save((err) => {
+      if (!err) {
+        console.log(`Notification job created: ${job.id}`);
+      } else {
+        console.error(`Error creating notification job: ${err}`);
+      }
+    });
+  });
+};
+
+export default createPushNotificationsJobs;

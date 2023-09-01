0a15fddd6068582241e149e4e6a76f182aa5149d
sofy <bedlusofi@gmail.com>
2023-09-01 09:10:30 +0300

    task-2

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,35 @@
+import redis from 'redis';
+import util from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log('Redis client not connected to the server:', error);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+};
+
+const getAsync = util.promisify(client.get).bind(client);
+
+const displaySchoolValue = async (schoolName) => {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const main = async () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+};
+
+main();
